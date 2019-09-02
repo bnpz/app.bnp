bd01ddeca62114e5eef2430904ad405f817a099f
bnpz <bnp.zenica.web@gmail.com>
2019-09-02 11:12:15 +0200

    database create

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render("homepage/index.html.twig");
+    }
+}
---
+++ src/Entity/Customer/Customer.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Entity\Customer;
+
+
+use App\Entity\Base\EntityInterface;
+
+class Customeri implements EntityInterface
+{
+
+}
