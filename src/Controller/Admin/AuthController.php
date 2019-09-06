f7172c4c5d354d0efec27b34b3e013003f0fc358
bnpz <bnp.zenica.web@gmail.com>
2019-09-06 11:22:55 +0200

    login remember me

--- src/Controller/Admin/AuthController.php
+++ src/Controller/Admin/AuthController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class AdminAuthController extends AbstractController
+class AuthController extends AbstractController
 {
     /**
      * @Route("/admin/login", name="app_admin_auth_login")
@@ -27,5 +27,13 @@ class AdminAuthController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    /**
+     * @Route("/admin/logout", name="app_admin_auth_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('Will be intercepted before getting here');
     }
 }
