47b030e1427c8e43abbd20f80ea294576a70a638
bnpz <PasvordGTHB#133>
2019-09-07 14:23:06 +0200

    User service init

--- src/Service/User/UserService.php
+++ src/Service/User/UserService.php
@@ -6,20 +6,40 @@ namespace App\Service\User;
 
 use App\Contract\Service\User\UserServiceInterface;
 use App\Entity\User\User;
+use App\Repository\User\UserRepository;
+use App\Service\Base\AbstractEntityService;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Security;
 
-class UserService implements UserServiceInterface
+class UserService extends AbstractEntityService implements UserServiceInterface
 {
+    /**
+     * @var UserRepository
+     */
+    protected $repository;
     /**
      * @var Security
      */
     private $security;
 
-    public function __construct(Security $security)
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        UserRepository $userRepository,
+        Security $security
+    )
     {
+        parent::__construct($managerRegistry);
+        $this->repository = $userRepository;
         $this->security = $security;
     }
 
+    /**
+     * @return string
+     */
+    protected function getEntityClassName()
+    {
+        return User::class;
+    }
     /**
      * @return User|null
      */
@@ -27,4 +47,15 @@ class UserService implements UserServiceInterface
     {
         return $this->security->getUser();
     }
+
+    /**
+     * @param string $email
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User
+    {
+        return $this->repository->findOneBy(['email' => $email]);
+    }
+
+
 }
