495931c46483955b2805f5a9d6104416351708ff
bnpz <bnp.zenica.web@gmail.com>
2019-09-17 11:14:36 +0200

    flash messages

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -7,5 +7,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as SymfonyAbstr
 
 abstract class AbstractController extends SymfonyAbstractController
 {
+    const FLASH_TYPE_SUCCESS    = "success";
+    const FLASH_TYPE_ERROR      = "error";
+    const FLASH_TYPE_NOTICE     = "notice";
 
+    /**
+     * @param string $message
+     */
+    protected function addFlashSuccess(string $message)
+    {
+        $this->addFlash(self::FLASH_TYPE_SUCCESS, $message);
+    }
+
+    /**
+     * @param string $message
+     */
+    protected function addFlashError(string $message)
+    {
+        $this->addFlash(self::FLASH_TYPE_ERROR, $message);
+    }
+
+    /**
+     * @param string $message
+     */
+    protected function addFlashNotice(string $message)
+    {
+        $this->addFlash(self::FLASH_TYPE_NOTICE, $message);
+    }
 }
