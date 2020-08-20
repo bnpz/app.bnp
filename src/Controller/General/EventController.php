82b513ee7342e75053af7d1047b4fcce87b25b92
bnpz <bnp.zenica.web@gmail.com>
2020-08-20 16:17:39 +0200

    filters to Events

--- src/Controller/General/EventController.php
+++ src/Controller/General/EventController.php
@@ -6,15 +6,12 @@ use App\Controller\AbstractController;
 use App\Entity\Base\EntityInterface;
 use App\Entity\General\Event;
 use App\Entity\General\Reservation;
+use App\Form\General\Event\EventFiltersType;
 use App\Form\General\EventReservationType;
 use App\Form\General\EventType;
-use App\Form\General\ReservationType;
-use App\Repository\General\EventRepository;
 use App\Service\General\EventService;
 use App\Service\General\ReservationService;
 use Doctrine\ORM\NonUniqueResultException;
-use Doctrine\ORM\OptimisticLockException;
-use Doctrine\ORM\ORMException;
 use Doctrine\ORM\Query\QueryException;
 use Exception;
 use Symfony\Component\HttpFoundation\Request;
@@ -35,43 +32,69 @@ class EventController extends AbstractController
      * @return Response
      * @throws QueryException
      */
-    public function index(
-        Request $request,
-        EventService $eventService,
-        int $page
-    ): Response
+    public function index(Request $request, EventService $eventService, int $page): Response
     {
+        $filters = $eventService->getFiltersFromSession();
+        $form = $this->createForm(EventFiltersType::class, null, [
+            'action' => $this->generateUrl('general_event_set_filters')
+        ]);
 
-            $paginator = $eventService->getNewPaginator(
+        $paginator = $eventService->getFilterPaginator(
+                $filters,
                 $page,
                 EntityInterface::PAGE_LIMIT,
                 $request->query->get('orderBy', 'time'),
-                $request->query->get('orderDirection', 'ASC')
+                $request->query->get('orderDirection', 'DESC')
             );
 
         $paginator->setRouteName('general_event_index_paginated');
 
         return $this->render('general/event/index.html.twig', [
             'events' => $paginator->getResults(),
-            'paginator' => $paginator
+            'paginator' => $paginator,
+            'filters' => $filters,
+            'filtersForm' => $form->createView()
         ]);
     }
-
     /**
-     * @Route("/old", name="general_event_index_old", methods={"GET"}, defaults={"page": "1"})
-     * @Route("/old/page/{page<[1-9]\d*>}", methods={"GET"}, name="general_event_index_old_paginated")
+     * @Route("/newEvents", name="general_event_index_new", methods={"GET"}, defaults={"page": "1"})
+     * @Route("/newEvents/page/{page<[1-9]\d*>}", methods={"GET"}, name="general_event_index_new_paginated")
      * @param Request $request
      * @param EventService $eventService
      * @param int $page
      * @return Response
      * @throws QueryException
      */
-    public function indexOld(
-        Request $request,
-        EventService $eventService,
-        int $page
-    ): Response
+    public function indexNew(Request $request, EventService $eventService, int $page): Response
     {
+        $paginator = $eventService->getNewPaginator(
+            $page,
+            EntityInterface::PAGE_LIMIT,
+            $request->query->get('orderBy', 'time'),
+            $request->query->get('orderDirection', 'ASC')
+        );
+
+        $paginator->setRouteName('general_event_index_new_paginated');
+
+        return $this->render('general/event/index.html.twig', [
+            'events' => $paginator->getResults(),
+            'paginator' => $paginator,
+            'filters' => false
+        ]);
+    }
+    /**
+     * @Route("/oldEvents", name="general_event_index_old", methods={"GET"}, defaults={"page": "1"})
+     * @Route("/oldEvents/page/{page<[1-9]\d*>}", methods={"GET"}, name="general_event_index_old_paginated")
+     * @param Request $request
+     * @param EventService $eventService
+     * @param int $page
+     * @return Response
+     * @throws QueryException
+     */
+    public function indexOld(Request $request, EventService $eventService, int $page): Response
+    {
+
+        $eventService->setFiltersToSession(['externalProduction' => 1]);
 
         $paginator = $eventService->getOldPaginator(
             $page,
@@ -84,7 +107,8 @@ class EventController extends AbstractController
 
         return $this->render('general/event/index.html.twig', [
             'events' => $paginator->getResults(),
-            'paginator' => $paginator
+            'paginator' => $paginator,
+            'filters' => false
         ]);
     }
     /**
@@ -113,7 +137,7 @@ class EventController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="general_event_show", methods={"GET"})
+     * @Route("/{id<[1-9]\d*>}", name="general_event_show", methods={"GET"})
      * @param Event $event
      * @param ReservationService $reservationService
      * @return Response
@@ -260,5 +284,33 @@ class EventController extends AbstractController
             $this->addFlashError($e->getMessage());
         }
         return $this->redirectToRoute('general_event_show', ['id' => $event->getId()]);
+    }
+
+    /**
+     * @Route("/clearFilters", name="general_event_clear_filters", methods={"GET"})
+     * @param Request $request
+     * @param EventService $eventService
+     * @return Response
+     */
+    public function resetFilters(Request $request, EventService $eventService): Response
+    {
+        $eventService->clearFiltersFromSession();
+        return $this->redirectToRoute('general_event_index');
+    }
+
+    /**
+     * @Route("/setFilters", name="general_event_set_filters", methods={"POST"})
+     * @param Request $request
+     * @param EventService $eventService
+     * @return Response
+     */
+    public function setFilters(Request $request, EventService $eventService): Response
+    {
+        $filters = $request->request->all('event_filters');
+        if(isset($filters['homeProduction'])) {
+            $filters['externalProduction'] = 0;
+        }
+        $eventService->setFiltersToSession($filters);
+        return $this->redirectToRoute('general_event_index');
     }
 }
